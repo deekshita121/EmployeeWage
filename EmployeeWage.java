5621d20e6ac2451d5ba56579b5edaff5b6670b50
Dee121-ala <deekshitaalavala@gmail.com>
2020-09-29 18:48:27 +0530

    [deekshita : Created a branch usecase 9 and merging with master]

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,7 +3,20 @@ public class EmployeeWage
  public static final int FULL_TIME=1;
  public static final int PART_TIME=2;
 
- public static int computeEmpWage(String company, int rate, int maxHrs, int maxDays)
+ private final String company;
+ private final int rate;
+ private final int maxHrs;
+ private final int maxDays;
+ private int totalEmpWage;
+
+ public EmployeeWage(String company, int rate, int maxDays, int maxHrs)
+ {
+  this.company=company;
+  this.rate=rate;
+  this.maxDays=maxDays;
+  this.maxHrs=maxHrs;
+ }
+ public void computeEmpWage( )
  {
   int totalEmphrs=0;
   int emphrs=0;
@@ -11,7 +24,7 @@ public class EmployeeWage
   while(emphrs<maxHrs && days<maxDays)
   {
    days++;
-   int check=(int)Math.floor(Math.random()*10 % 3);
+   int check=(int)Math.floor(Math.random()*10) % 3;
    switch(check)
    {
     case FULL_TIME:
@@ -26,15 +39,22 @@ public class EmployeeWage
    totalEmphrs +=emphrs;
    System.out.println("Day : "+days+" Emp hr : "+emphrs);
   }
-  int totalEmpwage = totalEmphrs * rate;
-  System.out.println("Total employee wage for company : "+company+" is "+totalEmpwage);
-  return totalEmpwage;
+  totalEmpWage = totalEmphrs * rate;
+ }
+
+ public String toString()
+ {
+  return "Total employee wage for company "+company+" is "+totalEmpWage;
  }
 
  public static void main(String args[])
  {
-   computeEmpWage("DMart", 20, 100, 20);
-   computeEmpWage("BigBazar", 30, 110, 18);
+  EmployeeWage obj1=new EmployeeWage("DMart",20,20,100);
+  EmployeeWage obj2=new EmployeeWage("Bigbazar",30,18,110);
+  obj1.computeEmpWage();
+  System.out.println(obj1);
+  obj2.computeEmpWage();
+  System.out.println(obj2);
  }
 
 }
